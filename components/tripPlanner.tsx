42791253f1e9373e09d2d1808fb637a62d769c0b
Andrew Derr <andrewderr2019@gmail.com>
2023-04-24 16:29:39 -0600

    Create trip planner class

---
+++ components/tripPlanner.tsx
@@ -0,0 +1,57 @@
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+  } from "use-places-autocomplete";
+  import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+  } from "@reach/combobox";
+  import "@reach/combobox/styles.css";
+  
+  type PlacesProps = {
+    setSearchResult: (position: google.maps.LatLngLiteral) => void;
+  };
+  
+  export default function TripPlanning({ setSearchResult }: PlacesProps) {
+    const {
+      ready,
+      value,
+      setValue,
+      suggestions: { status, data },
+      clearSuggestions,
+    } = usePlacesAutocomplete();
+  
+    const handleSelect = async (val: string) => {
+      setValue(val, false);
+      clearSuggestions();
+  
+      const results = await getGeocode({ address: val });
+      const { lat, lng } = await getLatLng(results[0]);
+      //Add this lat lng set to a list that we can then display below the search bar
+      setSearchResult({ lat, lng });
+    };
+  
+    return (
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={!ready}
+          className="combobox-input"
+          placeholder="Search office address"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ place_id, description }) => (
+                <ComboboxOption key={place_id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    );
+  }
+  
